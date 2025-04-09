dd80af9dcd15c0284201c1f022d620860093a6e3
Melanie Baker <104843873+Melanie-J-Baker@users.noreply.github.com>
2025-04-09 11:45:19 +0100

    Adjust project to work with AWS API Gateway/Lambda REST API backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,35 @@ import teaImage from './tea.png';
 function App() {
   const [userLocation, setUserLocation] = useState(null);
   const [teaLocations, setTeaLocations] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [locationsLoading, setLocationsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [apiKeyLoading, setApiKeyLoading] = useState(true);
   const [apiKey, setApiKey] = useState(null);
 
   useEffect(() => {
-    fetch('https://mel-baker.co.uk/find-me-tea/google-maps-api-key')
-      .then((response) => response.json())
-      .then((data) => setApiKey(data.apiKey))
-      .catch((err) => setError('Failed to load API key. ' + err));
+    fetch('https://api.mel-baker.co.uk/google-maps-api-key/')
+    .then((response) => {
+      console.log('API Key Response:', response);
+      if (!response.ok) {
+        throw new Error('Failed to fetch API key');
+      }
+      return response.json();
+    })
+      .then((data) => {
+         console.log('API Key Data:', data);
+         if (data && data.body.googleApiKey) {
+           setApiKey(data.body.googleApiKey);
+           setApiKeyLoading(false);
+         } else {
+           throw new Error('API key not found in the response');
+         }
+      })
+      .catch((err) => {
+        console.error('Error fetching API key:', err);
+        setError('Failed to load API key. ' + err)
+        setApiKeyLoading(false);
+      });
   }, [])
 
   const getLocationsOfTea = () => {
@@ -28,13 +47,12 @@ function App() {
           const { latitude, longitude } = position.coords;
           setUserLocation({ latitude, longitude});
           // fetch request to get locations of tea from Google Places API Nearby Search
-          setLoading(true);
+          setLocationsLoading(true);
           // send request to API
-          fetch('https://mel-baker.co.uk/find-me-tea/search', {
-            mode: 'cors',
+          fetch('https://api.mel-baker.co.uk/search/', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify({
               latitude,
@@ -44,9 +62,9 @@ function App() {
           .then((response) => {
             return response.json()
           })
-          .then((data) => setTeaLocations(data.places || []))
+          .then((data) => setTeaLocations(data.body.places || []))
           .catch((error) => setError(error.message))
-          .finally(() => setLoading(false))
+          .finally(() => setLocationsLoading(false))
         },
         (err) => {
           // display error if can't get user's position
@@ -65,37 +83,48 @@ function App() {
     }
   }
 
+  console.log('apiKeyLoading:', apiKeyLoading);
+  console.log('apiKey:', apiKey);
+  console.log('teaLocations:', teaLocations);
+  console.log('locationsLoading:', locationsLoading);
+  console.log('error:', error);
+
   return (
     <>
       <img src={teaImage} className='mainImage' alt='A cup of tea with a saucer'></img>
       <h1 className='mainHeading'>Find me tea</h1>
       <p className='subheading'>Find your nearest tea rooms, cafes, diners, and other places selling tea within a 2km radius</p>
       <button className='findMeTeaBtn' onClick={getLocationsOfTea}>
-        {loading ? "Finding..." : "Find tea near me"}
+        {locationsLoading ? "Finding..." : "Find tea near me"}
       </button>
-      {loading ? <h2>Loading...</h2> : teaLocations && (
-        <div className='locationsDiv'>
-          {userLocation && (
-            <APIProvider apiKey={apiKey}>
-              <div className='mapContainer'>
-                <Map
-                  defaultZoom={13}
-                  defaultCenter={{ lat: userLocation.latitude, lng: userLocation.longitude }}
-                  mapId='5a92b3e52305b0d4'
-                >
-                  <Markers teaLocations={teaLocations} handleLocationClick={handleLocationClick} selectedLocation={selectedLocation}/>
-                </Map>
+      {apiKeyLoading && <h2>Loading API Key...</h2>}
+      {locationsLoading && !apiKeyLoading ? (
+        <h2>Loading tea locations...</h2>
+      ) : (
+        apiKey && (
+          <div className='locationsDiv'>
+            {userLocation && (
+              <APIProvider apiKey={apiKey}>
+                <div className='mapContainer'>
+                  <Map
+                    defaultZoom={13}
+                    defaultCenter={{ lat: userLocation.latitude, lng: userLocation.longitude }}
+                    mapId='5a92b3e52305b0d4'
+                  >
+                    <Markers teaLocations={teaLocations} handleLocationClick={handleLocationClick} selectedLocation={selectedLocation}/>
+                  </Map>
+                </div>
+              </APIProvider>
+            )}
+            
+            {teaLocations && teaLocations.map((teaLocation) => (
+              <div className={selectedLocation === teaLocation.id ? "selectedLocationListItem" : "locationListItem"} key={teaLocation.id} onClick={() => handleLocationClick(teaLocation.id)}>
+                <p className="locationName">{teaLocation.displayName.text}</p>
+                <p className="locationAddress">{teaLocation.formattedAddress}</p>
               </div>
-            </APIProvider>
-          )}
-          
-          {teaLocations.map((teaLocation) => (
-            <div className={selectedLocation === teaLocation.id ? "selectedLocationListItem" : "locationListItem"} key={teaLocation.id} onClick={() => handleLocationClick(teaLocation.id)}>
-              <p className="locationName">{teaLocation.displayName.text}</p>
-              <p className="locationAddress">{teaLocation.formattedAddress}</p>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )
       )}
       {error && <h2>{error}</h2>}
     </>
